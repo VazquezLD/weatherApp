68aba012b67eda51e39b63a5902b1a0c76347ee5
Luciano <davlucho86gmail.com>
2024-05-10 00:00:23 -0300

    conección api/input, desestructuramiento de objeto

--- weatherApp/scripts/main.js
+++ weatherApp/scripts/main.js
@@ -1,22 +1,46 @@
-//Form ID
+/*CONSTANTES PARA OBJETOS DEL DOCUMENTO*/
 const form = document.getElementById('formulario');
 const input_ciudad = document.getElementById('inputciudad');
 const contenedor = document.querySelector('.containerCard');
 
+
+/*VALIDAR INPUTS*/
 const inputVacio = () =>{
     return input_ciudad.value.trim() == '';
 };
 
-const buscarCiudad = (e) =>{
-    e.preventDefault()
-    if (inputVacio()){
-        alert('Ingresa una ciuaaadad xfa :(');
-        return;
+/*DESESTRUCTURA EL OBJETO CIUDAD PARA MODIFICAR EL CONTENEDOR*/
+const obtenerData = (ciudad) =>{
+    return{
+        ciudadPais:ciudad.sys.country,
+        ciudadNombre:ciudad.name,
+        ciudadHumedad:ciudad.main.humidity + '%',
+        ciudadTemperatura:ciudad.main.temp,
+        ciudadTemperaturaMax:ciudad.main.temp_max,
+        ciudadTemperaturaMin:ciudad.main.temp_min,
+        ciudadAmanecer:ciudad.sys.sunrise,
+        ciudadAtardecer:ciudad.sys.sunset,
+        ciudadPron:ciudad.weather[0].main,
+        ciudadPronActual:ciudad.weather[0].description,
+        ciudadIcono:ciudad.weather[0].icon
     }
 }
 
-const init = () => {
-    form.addEventListener('submit', buscarCiudad);
+/*MANDA LA CIUDAD A LA FUNCION DE LA API*/
+const buscarCiudad = async(e) =>{
+    e.preventDefault()
+
+    if (inputVacio()){
+        alert('Ingresa una ciuaaadad xfa :(');
+        return;
+    };
+    const fecthCiudad = await requestCiudad(input_ciudad.value);
+    console.log(obtenerData(fecthCiudad));
 };
 
+/*INICIALIZACIÓN*/
+const init = () => {
+    form.addEventListener('submit', buscarCiudad)
+};
 init()
+
--- weatherApp/scripts/request.js
+++ weatherApp/scripts/request.js
@@ -1,11 +1,10 @@
 //Clave de la API//
-
 const apiKey = 'ad95c0f7fa0ebb8a799f997cb3c0eb06';
-const ciudad = 'Cordoba'
 
+/*UTILIZA EL PARAMETRO CIUDAD PARA BUSCARLO EN LOS DATOS DE LA API*/
 const requestCiudad = async(ciudad) =>{
     const respuesta = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&APPID=${apiKey}`);
     const data = await respuesta.json();
+    console.log(data);
     return data
 }
-requestCiudad(ciudad);
