cae3a56fb97b3501c4fc7f6a9d952272a9454059
Luciano <davlucho86@gmail.com>
2025-04-27 20:01:46 -0300

    progeso de funcionalidad aside

--- weatherApp/scripts/aside.js
+++ weatherApp/scripts/aside.js
@@ -1,9 +1,12 @@
 const listaFuncs = document.getElementById('listafuncs');
 const funciones = Array.from(listaFuncs.children);
+const contenedorGraficos = document.getElementById('contenedorGraficos')
+
 
 funciones.forEach(funcion => {
     funcion.addEventListener('click', () => {
       funciones.forEach(marcado => marcado.classList.remove('fondoFuncionalidad'))
       funcion.classList.add('fondoFuncionalidad')
+      contenedorGraficos.classList.add('oculto')
     })
   });
